021ed27ef17b654aac4dd37b686bd55077034c05
Aritra Das <aritrarick2002@gmail.com>
2021-09-22 11:15:44 +0530

    Java file updated , crashes fixed

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView but_1,but_2,but_3,but_4,but_5,but_6,but_7,but_8,but_9,but_0;
-    ImageView but_dot,button_plus,but_minus,but_cancel,but_mod,but_div,but_multiply,but_equal,all_clear,but_openbracket,but_closebracket;
+    ImageView bt_1,bt_2,bt_3,bt_4,bt_5,bt_6,bt_7,bt_8,bt_9,bt_0;
+    ImageView bt_dot, bt_equal, bt_ac, bt_module, bt_plus, bt_minus, bt_multiply, bt_cancel,bt_divide;
 
     TextView inputTxt,outputTxt;
     String data;
@@ -22,268 +26,178 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inputTxt=findViewById(R.id.inputTxt);
-        outputTxt=findViewById(R.id.outputTxt);
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        but_0=findViewById(R.id.but_zero);
-        but_1=findViewById(R.id.but_1);
-        but_2=findViewById(R.id.but_2);
-        but_3=findViewById(R.id.but_3);
-        but_4=findViewById(R.id.but_4);
-        but_5=findViewById(R.id.but_5);
-        but_6=findViewById(R.id.but_6);
-        but_7=findViewById(R.id.but_7);
-        but_8=findViewById(R.id.but_8);
-        but_9=findViewById(R.id.but_9);
+        inputTxt = findViewById(R.id.inputTxt);
+        outputTxt = findViewById(R.id.outputTxt);
 
-        but_dot=findViewById(R.id.but_dot);
-        button_plus=findViewById(R.id.but_plus);
-        but_minus=findViewById(R.id.but_minus);
-        but_cancel=findViewById(R.id.but_cancel);
-        but_mod=findViewById(R.id.but_mod);
-        but_div=findViewById(R.id.but_div);
-        but_multiply=findViewById(R.id.but_multiply);
-        but_equal=findViewById(R.id.but_equal);
-        but_openbracket=findViewById(R.id.open_brack);
-        but_closebracket=findViewById(R.id.close_brack);
-        all_clear=findViewById(R.id.all_clear);
+        bt_0 = findViewById(R.id.but_0);
+        bt_1 = findViewById(R.id.but_1);
+        bt_2 = findViewById(R.id.but_2);
+        bt_3 = findViewById(R.id.but_3);
+        bt_4 = findViewById(R.id.but_4);
+        bt_5 = findViewById(R.id.but_5);
+        bt_6 = findViewById(R.id.but_6);
+        bt_7 = findViewById(R.id.but_7);
+        bt_8 = findViewById(R.id.but_8);
+        bt_9 = findViewById(R.id.but_9);
 
-        but_0.setOnClickListener(new View.OnClickListener() {
+        bt_dot = findViewById(R.id.but_dot);
+        bt_equal = findViewById(R.id.but_equal);
+        bt_ac = findViewById(R.id.all_clear);
+        bt_module = findViewById(R.id.but_mod);
+        bt_plus = findViewById(R.id.but_plus);
+        bt_minus = findViewById(R.id.but_minus);
+        bt_multiply = findViewById(R.id.but_multiply);
+        bt_cancel = findViewById(R.id.but_cancel);
+        bt_divide = findViewById(R.id.but_div);
+
+        bt_0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"0");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "0");
             }
         });
-        but_1.setOnClickListener(new View.OnClickListener() {
+        bt_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"1");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "1");
             }
         });
-        but_2.setOnClickListener(new View.OnClickListener() {
+        bt_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"2");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "2");
             }
         });
-        but_3.setOnClickListener(new View.OnClickListener() {
+        bt_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"3");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "3");
             }
         });
-        but_4.setOnClickListener(new View.OnClickListener() {
+        bt_4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"4");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "4");
             }
         });
-        but_5.setOnClickListener(new View.OnClickListener() {
+        bt_5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"5");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "5");
             }
         });
-        but_6.setOnClickListener(new View.OnClickListener() {
+        bt_6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"6");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "6");
             }
         });
-        but_7.setOnClickListener(new View.OnClickListener() {
+        bt_7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"7");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "7");
             }
         });
-        but_8.setOnClickListener(new View.OnClickListener() {
+        bt_8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"8");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "8");
             }
         });
-        but_9.setOnClickListener(new View.OnClickListener() {
+        bt_9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"9");
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "9");
             }
         });
-        but_dot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+".");
-            }
-        });
-        button_plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"+");
-            }
-        });
-        but_minus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"-");
-            }
-        });
-        but_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                data = data.substring(0, data.length() -1);
-                inputTxt.setText(data);
-            }
-        });
-        but_mod.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"%");
-            }
-        });
-        but_multiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"✕");
-            }
-        });
-        but_div.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"÷");
-            }
-        });
-        but_openbracket.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+"(");
-            }
-        });
-        but_closebracket.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                data=inputTxt.getText().toString();
-                inputTxt.setText(data+")");
-            }
-        });
-        all_clear.setOnClickListener(new View.OnClickListener() {
+        bt_ac.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 inputTxt.setText("");
                 outputTxt.setText("");
             }
         });
-        but_equal.setOnClickListener(new View.OnClickListener() {
+        bt_plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 data = inputTxt.getText().toString();
-                data = data.replaceAll("X", "*");
+                inputTxt.setText(data + "+");
+            }
+        });
+        bt_dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + ".");
+            }
+        });
+        bt_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "-");
+            }
+        });
+        bt_module.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "%");
+            }
+        });
+        bt_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = inputTxt.getText().toString();
+                data = data.substring(0,data.length()-1);
+                inputTxt.setText(data);
+            }
+        });
+        bt_divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "÷");
+            }
+        });
+        bt_multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = inputTxt.getText().toString();
+                inputTxt.setText(data + "×");
+            }
+        });
+        bt_equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = inputTxt.getText().toString();
+
+                data = data.replaceAll("×","*");
                 data = data.replaceAll("%","/100");
                 data = data.replaceAll("÷","/");
-                double result = eval(data);
-                inputTxt.setText(String.valueOf(result));
-                outputTxt.setText(data);
-            }
 
-            
+                Context rihno = Context.enter();
+                rihno.setOptimizationLevel(-1);
+
+                String finalRes ="";
+                Scriptable scriptable = rihno.initStandardObjects();
+                finalRes = rihno.evaluateString(scriptable,data,"Javascript",1,null).toString();
+
+                outputTxt.setText(finalRes);
+            }
         });
-
-    }
-    public static double eval(final String str) {
-        return new Object() {
-            int pos = -1, ch;
-
-            void nextChar() {
-                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
-            }
-
-            boolean eat(int charToEat) {
-                while (ch == ' ') nextChar();
-                if (ch == charToEat) {
-                    nextChar();
-                    return true;
-                }
-                return false;
-            }
-
-            double parse() {
-                nextChar();
-                double x = parseExpression();
-                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
-                return x;
-            }
-
-            // Grammar:
-            // expression = term | expression `+` term | expression `-` term
-            // term = factor | term `*` factor | term `/` factor
-            // factor = `+` factor | `-` factor | `(` expression `)`
-            //        | number | functionName factor | factor `^` factor
-
-            double parseExpression() {
-                double x = parseTerm();
-                for (;;) {
-                    if      (eat('+')) x += parseTerm(); // addition
-                    else if (eat('-')) x -= parseTerm(); // subtraction
-                    else return x;
-                }
-            }
-
-            double parseTerm() {
-                double x = parseFactor();
-                for (;;) {
-                    if      (eat('*')) x *= parseFactor(); // multiplication
-                    else if (eat('/')) x /= parseFactor(); // division
-                    else return x;
-                }
-            }
-
-            double parseFactor() {
-                if (eat('+')) return parseFactor(); // unary plus
-                if (eat('-')) return -parseFactor(); // unary minus
-
-                double x;
-                int startPos = this.pos;
-                if (eat('(')) { // parentheses
-                    x = parseExpression();
-                    eat(')');
-                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
-                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
-                    x = Double.parseDouble(str.substring(startPos, this.pos));
-                } else if (ch >= 'a' && ch <= 'z') { // functions
-                    while (ch >= 'a' && ch <= 'z') nextChar();
-                    String func = str.substring(startPos, this.pos);
-                    x = parseFactor();
-                    if (func.equals("sqrt")) x = Math.sqrt(x);
-                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-                    else if (func.equals("log")) x = Math.log10(x);
-                    else if (func.equals("ln")) x = Math.log(x);
-                    else throw new RuntimeException("Unknown function: " + func);
-                } else {
-                    throw new RuntimeException("Unexpected: " + (char)ch);
-                }
-
-                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
-
-                return x;
-            }
-        }.parse();
     }
 }
